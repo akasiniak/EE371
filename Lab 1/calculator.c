4ae5783e8f0d0605edf9370bfe975d5d050474b3
akasiniak <alex.kasiniak@gmail.com>
2018-01-09 13:30:40 -0800

    Finished the calculator

--- Lab 1/calculator.c
+++ Lab 1/calculator.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 int main(void) {
   float conversionRate, dollarAmount, convertedAmount;
-  int conversionDirection;
+  char conversionDirection;
   printf("What is your conversion rate (from dollars to local)?");
   printf("\n");
   scanf("%f", &conversionRate);
+  getchar();
   printf("Which direction are you converting?");
   printf("\n");
   printf("(l for local -> dollars, d for dollars -> local)");
   printf("\n");
-  scanf("%d", &conversionDirection);
-  printf("\n");
+  scanf("%c", &conversionDirection);
+  getchar();
   printf("How much cash on hand do you have?");
   printf("\n");
   scanf("%f", &dollarAmount);
+  getchar();
   if(conversionDirection == 'd') {
       convertedAmount = dollarAmount * conversionRate;
   } else {
