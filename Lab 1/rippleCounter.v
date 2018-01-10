4f31bcd0cf210abee4c0171dfbf32b8d565d142a
akasiniak <alex.kasiniak@gmail.com>
2018-01-09 20:03:07 -0800

    Updated rippleCounter

--- Lab 1/rippleCounter.v
+++ Lab 1/rippleCounter.v
@@ -5,15 +5,10 @@ module rippleCounter(count, clk, reset);
   output [3:0] count;
   wire [3:0] q, qBar;
   genvar i;
-  DFlipFlop zerothFlipFlop(q[0], qBar[0], qBar[0], clk, reset); //set up the flip flops
+  DFlipFlop zerothFlipFlop(q[0], count[0], count[0], clk, reset); //set up the flip flops
   generate
     for(i = 1; i < 4; i = i + 1) begin : eachFlipFlop
-      DFlipFlop nthFlipFlop(q[i], qBar[i], qBar[i], q[i - 1], reset);
+      DFlipFlop nthFlipFlop(q[i], count[i], count[i], q[i - 1], reset);
     end
   endgenerate
-  //assign the output to the qbar of each flip flop
-  assign count[0] = qBar[0];
-  assign count[1] = qBar[1];
-  assign count[2] = qBar[2];
-  assign count[3] = qBar[3];
 endmodule
