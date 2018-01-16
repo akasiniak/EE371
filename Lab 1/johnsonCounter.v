e804fb073fe547f8b7c7dd4a441df37d8c673b4b
akasiniak <alex.kasiniak@gmail.com>
2018-01-16 11:03:30 -0800

    I think I finished the johnson counter

--- Lab 1/johnsonCounter.v
+++ Lab 1/johnsonCounter.v
@@ -1,4 +1,41 @@
 module johnsonCounter(count, clk, reset);
   input clk, reset;
-  output [3:0] count;
+  output reg [3:0] count;
+
+  always @ (posedge clk) begin
+    if(!reset) begin
+      count[3:0] <= 4'd0;
+    end else begin
+      count[0] <= ~count[3];
+      count[1] <= count[0];
+      count[2] <= count[1];
+      count[3] <= count[2];
+    end
+  end
+endmodule
+
+module johnsonCounter_testbench;
+  reg clk, rst;
+  wire [3:0] count;
+  johnsonCounter myJohnsonCounter(count, clk, rst);
+  parameter ClockDelay = 1;
+  initial begin //setup the clock
+    clk <= 0;
+    forever begin
+      #(ClockDelay);
+      clk <= ~clk;
+    end
+  end
+  integer i;
+  initial begin
+    $dumpfile("johnsonCounter.vcd");
+    $dumpvars(1, myJohnsonCounter);
+    rst = 1'b0; #ClockDelay;
+    #ClockDelay;
+    rst = 1'b1; #ClockDelay;
+    for(i = 0; i < 32; i++) begin
+      #ClockDelay; //run 32 clock cycles
+    end
+    $finish;
+  end
 endmodule
