8a1e7750967818693c5036deef2836a40dbb48a5
akasiniak <alex.kasiniak@gmail.com>
2018-01-16 19:26:10 -0800

    Finished up the report

--- Lab 1/calculator.c
+++ Lab 1/calculator.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 //Currency conversion calculator. Takes in information from the user and returns
-//a converted amount. Since we are using floats to keep hold of our variables, there
+//a converted amount. Since we are using doubles to keep hold of our variables, there
 //are bounds on the amounts that you can calculate. The largest amount however, is
 //3.40282 * 10^38, which is well above all of the money in the world, so going over
 //the limit should not be a concern.
 int main(void) {
-  float conversionRate, dollarAmount, convertedAmount;
+  float conversionRate;
+  double dollarAmount, convertedAmount;
   char conversionDirection;
   printf("What is your conversion rate (from dollars to local)?");
   printf("\n");
@@ -19,14 +20,14 @@ int main(void) {
   getchar();
   printf("How much cash on hand do you have?");
   printf("\n");
-  scanf("%f", &dollarAmount);
+  scanf("%lf", &dollarAmount);
   getchar();
   if(conversionDirection == 'd') {
       convertedAmount = dollarAmount * conversionRate;
   } else {
       convertedAmount = dollarAmount * (1/conversionRate);
   }
-  printf("Your converted amount: %.2f", convertedAmount);
+  printf("Your converted amount: %.2lf", convertedAmount);
   printf("\n");
   return 0;
 }
