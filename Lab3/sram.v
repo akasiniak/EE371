dc15bb30e3f9abbc9ce0fc55b053020a52bd1489
akasiniak <alex.kasiniak@gmail.com>
2018-02-12 18:05:50 -0800

    SRAM

--- Lab3/sram.v
+++ Lab3/sram.v
@@ -1,13 +1,13 @@
 /*  This module implements SRAM based CY7C128A architecture
     that can hold approximately 16k bits (2k X 8) and has
-    active low chip enable, active low write neable, and
-    active low output enable. */
+    active low chip enable, active low write, and
+    active low enable. */
 
-module sram(data_out, data_in, addr, chipEnable, writeEnable, outputEnable)
-  output reg  [7:0] data_out;
-  input wire  [7:0] data_in;
-  input wire  [10:0] addr;
-  input wire  chipEnable, writeEnable, outputEnable;
+module sram(data_out, data_in, addr, chipEnable, writeEnable, outputEnable);
+  output  [7:0]   data_out;
+  input   [7:0]   data_in;
+  input   [10:0]  addr;
+  input   chipEnable, writeEnable, outputEnable;
 
   /* 2048 X 8 RAM architecture */
   reg [7:0] mem [2047:0];
@@ -38,45 +38,45 @@ module sram_testbench;
     $dumpfile("sram.vcd");
     $dumpvars(1, dut);
     // Test 1: Should output all X's.
-    #10
+    #10;
     dataIn = 8'b11111111;
     addr = 11'b00000000000;
     chipEnable = 1'b0;
     writeEnable = 1'b1;
-    #10
+    #10;
     addr = 11'b00000000000;
     chipEnable = 1'b1;
     writeEnable = 1'b0;
     outputEnable = 1'b1; // Should output all X's.
-    #10
+    #10;
     outputEnable = 1'b0;
     // Test 2: Should output all X's.
-    #10
+    #10;
     dataIn = 8'b11111111;
     addr = 11'b00000000000;
     chipEnable = 1'b1;
     writeEnable = 1'b0;
-    #10
+    #10;
     addr = 11'b00000000000;
     chipEnable = 1'b1;
     outputEnable = 1'b1; // Should output all X's.
-    #10
+    #10;
     outputEnable = 1'b0;
-    #10
+    #10;
     // Test 3: Should output all X's or 1's depending on memory address.
     dataIn = 8'b11111111;
     addr = 11'b00000000000;
     chipEnable = 1'b1;
     writeEnable = 1'b1;
-    #10
+    #10;
     addr = 11'b11111111111;
     writeEnable = 1'b0;
     outputEnable = 1'b1; // Should output all X's.
-    #10
+    #10;
     addr = 11'b00000000000; // Should output all 1's.
-    #10
+    #10;
     outputEnable = 1'b0;
-    #10
+    #10;
   end
 
 endmodule
