38755be5dbca162ebd78a5932733d78fc42dba23
akasiniak <alex.kasiniak@gmail.com>
2018-02-12 18:52:07 -0800

    Tested SRAM. Works.

--- Lab3/sram.v
+++ Lab3/sram.v
@@ -4,10 +4,10 @@
     active low enable. */
 
 module sram(data_out, data_in, addr, chipEnable, writeEnable, outputEnable);
-  output  [7:0]   data_out;
-  input   [7:0]   data_in;
-  input   [10:0]  addr;
-  input   chipEnable, writeEnable, outputEnable;
+  output reg  [7:0]   data_out;
+  input wire  [7:0]   data_in;
+  input wire  [10:0]  addr;
+  input wire  chipEnable, writeEnable, outputEnable;
 
   /* 2048 X 8 RAM architecture */
   reg [7:0] mem [2047:0];
@@ -27,55 +27,60 @@ endmodule
 /* Test writing to and reading from SRAM. */
 
 module sram_testbench;
-  reg   [7:0] data_out;
-  wire  [7:0] data_in;
-  wire  [10:0] addr;
-  wire  chipEnable, writeEnable, outputEnable;
+  wire  [7:0] data_out;
+  reg   [7:0] data_in;
+  reg   [10:0] addr;
+  reg   chipEnable, writeEnable, outputEnable;
 
   sram sram_test(data_out, data_in, addr, chipEnable, writeEnable, outputEnable);
 
   initial begin
     $dumpfile("sram.vcd");
-    $dumpvars(1, dut);
+    $dumpvars(1, sram_test);
     // Test 1: Should output all X's.
     #10;
-    dataIn = 8'b11111111;
+    data_in = 8'b11111111;
     addr = 11'b00000000000;
     chipEnable = 1'b0;
     writeEnable = 1'b1;
+    outputEnable = 1'b1;
     #10;
-    addr = 11'b00000000000;
-    chipEnable = 1'b1;
-    writeEnable = 1'b0;
-    outputEnable = 1'b1; // Should output all X's.
-    #10;
-    outputEnable = 1'b0;
-    // Test 2: Should output all X's.
-    #10;
-    dataIn = 8'b11111111;
-    addr = 11'b00000000000;
-    chipEnable = 1'b1;
-    writeEnable = 1'b0;
-    #10;
-    addr = 11'b00000000000;
-    chipEnable = 1'b1;
-    outputEnable = 1'b1; // Should output all X's.
-    #10;
-    outputEnable = 1'b0;
-    #10;
-    // Test 3: Should output all X's or 1's depending on memory address.
-    dataIn = 8'b11111111;
     addr = 11'b00000000000;
     chipEnable = 1'b1;
     writeEnable = 1'b1;
+    outputEnable = 1'b0; // Should output all X's.
+    #10;
+    outputEnable = 1'b1;
+    // Test 2: Should output all X's.
+    #10;
+    data_in = 8'b11111111;
+    addr = 11'b00000000000;
+    chipEnable = 1'b1;
+    writeEnable = 1'b0;
+    outputEnable = 1'b1;
+    #10;
+    addr = 11'b00000000000;
+    chipEnable = 1'b1;
+    writeEnable = 1'b1;
+    outputEnable = 1'b0; // Should output all X's.
+    #10;
+    outputEnable = 1'b1;
+    #10;
+    // Test 3: Should output all X's or 1's depending on memory address.
+    data_in = 8'b11111111;
+    addr = 11'b00000000000;
+    chipEnable = 1'b0;
+    writeEnable = 1'b0;
+    outputEnable = 1'b1;
     #10;
     addr = 11'b11111111111;
-    writeEnable = 1'b0;
-    outputEnable = 1'b1; // Should output all X's.
+    chipEnable = 1'b0;
+    writeEnable = 1'b1;
+    outputEnable = 1'b0; // Should output all X's.
     #10;
     addr = 11'b00000000000; // Should output all 1's.
     #10;
-    outputEnable = 1'b0;
+    outputEnable = 1'b1;
     #10;
   end
 
